77d68acfe02db56a1b66bfcffb7822535d462e70
Stanislaw Blicharski <mainly.agn@gmail.com>
2025-04-17 19:14:27 +0200

    vercel

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type React from "react"
 import "./globals.css"
 import type { Metadata } from "next"
 import { Inter } from "next/font/google"
+import { Analytics } from "@vercel/analytics/react"
 
 const inter = Inter({ subsets: ["latin"] })
 
